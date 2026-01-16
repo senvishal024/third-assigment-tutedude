3a2fb9b699dcea0cde728d22df74b7db13f9371c
senvishal024 <senvishal024@gmail.com>
2026-01-16 09:19:24 +0530

    assigment 3 file

---
+++ script.js
@@ -0,0 +1,28 @@
+let headerText = document.querySelector('.header-text');
+let redBox =document.querySelector('#btn1');
+let blueBox = document.querySelector('#btn2');
+let greenBox =document.querySelector('#btn3');
+let yellowBox =document.querySelector('#btn4');
+
+let inputText =document.querySelector('#form-text');
+let greetBtn =document.querySelector('#greet-btn');
+
+redBox.addEventListener('click',()=>{
+
+    redBox.classList.toggle('active');
+});
+blueBox.addEventListener('click',()=>{
+
+    blueBox.classList.toggle('active');
+});
+greenBox.addEventListener('click',()=>{
+
+    greenBox.classList.toggle('active');
+});
+yellowBox.addEventListener('click',()=>{
+
+    yellowBox.classList.toggle('active');
+});
+greetBtn.addEventListener('click', ()=>{
+    headerText.textContent = inputText.value;
+});
